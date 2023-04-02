b2660101d2aa13cebb265d93c39f5cec0677ebe2
joinjun <dignzh@naver.com>
2023-04-02 17:47:16 +0900

    Initial commit

---
+++ src/test/java/com/example/testproject5/TestProject5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testproject5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProject5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
